6ecb16a1fa391a58f9155d0b813c16821719d76a
SupKelelawar <138984997+SupKelelawar@users.noreply.github.com>
2024-12-02 17:22:41 +0700

    Update SirenKomik.kt

--- src/id/mangkomik/src/eu/kanade/tachiyomi/extension/id/mangkomik/SirenKomik.kt
+++ src/id/mangkomik/src/eu/kanade/tachiyomi/extension/id/mangkomik/SirenKomik.kt
@@ -26,7 +26,7 @@ class SirenKomik :
 
     override val seriesTitleSelector = "h1.judul-komik"
     override val seriesThumbnailSelector = ".gambar-kecil img"
-    override val seriesAuthorSelector = ".keterangan-komik:contains(author) span"
+    override val seriesAuthorSelector = ".keterangan-komik:contains(Penulis) span"
     override val seriesArtistSelector = ".keterangan-komik:contains(artist) span"
 
     override fun chapterListSelector() = ".list-chapter a"
