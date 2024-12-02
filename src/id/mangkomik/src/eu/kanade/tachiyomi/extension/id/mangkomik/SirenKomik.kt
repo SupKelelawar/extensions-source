e610eae3ae08301f6a0324add47971dfddd38a1f
SupKelelawar <138984997+SupKelelawar@users.noreply.github.com>
2024-12-02 17:12:48 +0700

    Update SirenKomik.kt

--- src/id/mangkomik/src/eu/kanade/tachiyomi/extension/id/mangkomik/SirenKomik.kt
+++ src/id/mangkomik/src/eu/kanade/tachiyomi/extension/id/mangkomik/SirenKomik.kt
@@ -4,48 +4,60 @@ import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
-import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import okhttp3.Request
+import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class SirenKomik : MangaThemesia(
+class SirenKomik :
+    MangaThemesia(
     "Siren Komik",
     "https://sirenkomik.my.id",
     "id",
     dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("id")),
-) {
-    override val id = 8457447675410081142
+    ) {
+
+        override val id = 8457447675410081142
 
     override val hasProjectPage = true
 
     override val seriesTitleSelector = "h1.judul-komik"
     override val seriesThumbnailSelector = ".gambar-kecil img"
-    override val seriesGenreSelector = ".genre-komik a"
     override val seriesAuthorSelector = ".keterangan-komik:contains(author) span"
     override val seriesArtistSelector = ".keterangan-komik:contains(artist) span"
 
+    override fun chapterListSelector() = ".list-chapter a"
+
     override fun chapterFromElement(element: Element) = SChapter.create().apply {
-        val urlElements = element.select("a")
-        setUrlWithoutDomain(urlElements.attr("href"))
-        name = element.select(".nomer-chapter").text().ifBlank { urlElements.first()!!.text() }
+        name = element.selectFirst(".nomer-chapter")!!.text()
         date_upload = element.selectFirst(".tgl-chapter")?.text().parseChapterDate()
+        setUrlWithoutDomain(element.absUrl("href"))
     }
 
     override fun pageListParse(document: Document): List<Page> {
-        // Get external JS for image urls
-        val scriptEl = document.selectFirst("script[data-minify]")
-        val scriptUrl = scriptEl?.attr("src")
-        if (scriptUrl.isNullOrEmpty()) {
-            return super.pageListParse(document)
+        val scriptContent = document.selectFirst("script:containsData(SExtras.imgview)")?.data()
+            ?: return super.pageListParse(document)
+
+        val jsonString = scriptContent.substringAfter("SExtras.imgview(").substringBefore(");")
+        val sExtrasData = json.decodeFromString<SExtras>(jsonString)
+        val imageUrls: List<String> = sExtrasData.sources.firstOrNull()?.images ?: return emptyList()
+
+        return imageUrls.mapIndexed { index, imageUrl -> 
+            Page(index, document.location(), imageUrl)
         }
-
-        val scriptResponse = client.newCall(
-            GET(scriptUrl, headers),
-        ).execute()
-
-        // Inject external JS
-        scriptEl.text(scriptResponse.body.string())
-        return super.pageListParse(document)
     }
+
+    @Serializable
+    data class SExtras(
+        val sources: List<ImageSource>,
+    )
+
+    @Serializable
+    data class ImageSource(
+        val source: String,
+        val images: List<String>,
+    )
 }
