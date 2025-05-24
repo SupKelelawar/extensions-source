8277cdfed40890eee057476aca123f9a4e95771b
SupKelelawar <138984997+SupKelelawar@users.noreply.github.com>
2025-05-24 16:30:30 +0700

    Update Noromax.kt

--- src/id/noromax/src/eu/kanade/tachiyomi/extension/id/noromax/Noromax.kt
+++ src/id/noromax/src/eu/kanade/tachiyomi/extension/id/noromax/Noromax.kt
@@ -8,6 +8,20 @@ class Noromax : MangaThemesia(
     "id",
 ) {
 
+    override fun pageListParse(document: Document): List<Page> {
+    val resizeServiceUrl = getResizeServiceUrl()
+    val imageElements = document.select("#readerarea img")
+    return imageElements.mapNotNullIndexed { index, element ->
+        val imageUrl = element.absUrl("src")
+        // Kecualikan gambar yang berakhiran 999.png
+        if (imageUrl.endsWith("999.png")) {
+            null
+        } else {
+            Page(index, document.location(), "${resizeServiceUrl ?: ""}$imageUrl")
+        }
+    }
+}
+
     // Site changed from ZeistManga to MangaThemesia
     override val versionId = 2
 
