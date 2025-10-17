e941b792247410a78d594d688b001e0a502bdbda
SupKelelawar <138984997+SupKelelawar@users.noreply.github.com>
2025-10-17 21:26:00 +0700

    Add id.ngomik.cloud

---
+++ src/id/ngomikid/src/eu/kanade/tachiyomi/extension/id/ngomikid/NgomikID.kt
@@ -0,0 +1,12 @@
+package eu.kanade.tachiyomi.extension.id.ngomikid
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class NgomikID : MangaThemesia(
+    "NgomikID", 
+    "https://id.ngomik.cloud",
+    "id") {
+    dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale("id"))
+}
